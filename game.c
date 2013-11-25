dc50c01a15f12d74376646b1d7b0804a88715e03
yzhao1 <yzhao1@macalester.edu>
2013-11-25 14:46:52 -0600

    first commit

---
+++ game.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void fillboard(int *board, int dim){
+	int i, j;
+	for(i=0; i<dim; i++){
+		for(j=0; j<dim; j++){
+			board[i*dim+j] = rand
+		}
+	}
+}
+
+int main(void){
+	int n;
+	int *board;
+	board = (int*)malloc(our_number_of_people * sizeof(int));
+	srand(time(NULL));
+}
